c6cec8ac111a983c3ab9c4a879d48df20770791a
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-11-10 00:41:36 +0500

    init files

---
+++ price/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Price update',
+    'version': '1.1',
+    'author': 'Ilyas',
+    'category': 'Accounting & Finance',
+    'description': """ """,
+    'website': '',
+    'depends': ['base_action_rule'],
+    'data': [
+        'rule.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
