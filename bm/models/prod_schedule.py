a5385b743abd7188a137e26c724e99a2d09a6a17
ilyasprogrammer <rakhimkulov@it-projects.info>
2016-11-17 21:15:21 +0500

    spj views

--- bm/models/prod_schedule.py
+++ bm/models/prod_schedule.py
@@ -9,16 +9,16 @@ _logger = logging.getLogger(__name__)
 
 class ScheduleOfProductionJobs(models.Model):
     _name = 'bm.spj'
-    _description = 'График производственных работ'
+    _description = 'Schedule Of Production Jobs'
 
     project_local_id = fields.Many2one('bm.project')  # привязка может быть только по одному из этих полей
     project_external_id = fields.Many2one('bm.project')
-    estimate_ids = fields.One2many('bm.estimate', 'spj_id', string='Сметы')
-    currency_id = fields.Many2one('res.currency', string='Валюта', required=True,
+    estimate_ids = fields.One2many('bm.estimate', 'spj_id', string='Estimates')
+    currency_id = fields.Many2one('res.currency', string='Currency', required=True,
                                   default=lambda self: self.env.user.company_id.currency_id)
-    amount_labor = fields.Float(string='Плановые трудозатраты')
-    amount_mech = fields.Float(string='Плановые мех. часы')
-    amount_est_cost = fields.Float(string='Плановая стоимость')
+    amount_labor = fields.Float(string='Plan labor')
+    amount_mech = fields.Float(string='Plan mechanical hours')
+    amount_est_cost = fields.Float(string='Plan estimation cost')
     type = fields.Selection([('external', 'Внешний'),
                              ('local', 'Внутренний'),
                              ], 'Статус', readonly=True, default='local')
