6b18afb1ef474b4c630934eaadf203856394cec9
ilyasprogrammer <rakhimkulov@it-projects.info>
2016-11-18 19:46:58 +0500

    views imps

--- bm/models/prod_schedule.py
+++ bm/models/prod_schedule.py
@@ -13,15 +13,15 @@ class ScheduleOfProductionJobs(models.Model):
 
     project_local_id = fields.Many2one('bm.project')  # привязка может быть только по одному из этих полей
     project_external_id = fields.Many2one('bm.project')
-    estimate_ids = fields.One2many('bm.estimate', 'spj_id', string='Estimates')
-    currency_id = fields.Many2one('res.currency', string='Currency', required=True,
+    estimate_ids = fields.One2many('bm.estimate', 'spj_id', string='Сметы')
+    currency_id = fields.Many2one('res.currency', string='Валюта', required=True,
                                   default=lambda self: self.env.user.company_id.currency_id)
-    amount_labor = fields.Float(string='Plan labor')
-    amount_mech = fields.Float(string='Plan mechanical hours')
-    amount_est_cost = fields.Float(string='Plan estimation cost')
+    amount_labor = fields.Float(string='План трудозатраты')
+    amount_mech = fields.Float(string='План мех. часы')
+    amount_est_cost = fields.Float(string='План стоимость')
     type = fields.Selection([('external', 'Внешний'),
                              ('local', 'Внутренний'),
-                             ], 'Статус', readonly=True, default='local')
+                             ], 'Тип', default='local')
     state = fields.Selection([('draft', 'Черновик'),
                               ('approved', 'Утвержден'),
                               ('current', 'Текущий'),
