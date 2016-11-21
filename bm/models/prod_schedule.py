d62e46157a67f15e6327f0608ab22d97db781df0
ilyasprogrammer <rakhimkulov@it-projects.info>
2016-11-21 21:46:24 +0500

    gpr

--- bm/models/prod_schedule.py
+++ bm/models/prod_schedule.py
@@ -13,12 +13,11 @@ class ScheduleOfProductionJobs(models.Model):
 
     name = fields.Char()
     project_id = fields.Many2one('bm.project', string='Проект')
-    estimate_ids = fields.One2many('bm.estimate', 'spj_id', string='Сметы')
-    currency_id = fields.Many2one('res.currency', string='Валюта', required=True,
-                                  default=lambda self: self.env.user.company_id.currency_id)
+    currency_id = fields.Many2one('res.currency', string='Валюта', required=True, default=lambda self: self.env.user.company_id.currency_id)
     amount_labor = fields.Float(string='План трудозатраты')
     amount_mech = fields.Float(string='План мех. часы')
     amount_est_cost = fields.Float(string='План стоимость')
+    lines = fields.One2many('bm.spj.line', 'spj_id')
     type = fields.Selection([('external', 'Внешний'),
                              ('local', 'Внутренний'),
                              ], 'Тип', default='local')
@@ -37,3 +36,19 @@ class ScheduleOfProductionJobs(models.Model):
             vals['name'] = "ГПР " + type
         result = super(ScheduleOfProductionJobs, self).create(vals)
         return result
+
+
+class SPJLine(models.Model):
+    _name = 'bm.spj.line'
+    _description = 'SPJ lines'
+
+    spj_id = fields.Many2one('bm.spj')
+    estimate_id = fields.Many2one('bm.estimate')
+    type = fields.Selection([('labor', 'Труд'),
+                             ('mech', 'Машины'),
+                             ], default='labor')
+    month1 = fields.Float()
+    month2 = fields.Float()
+    month3 = fields.Float()
+    month4 = fields.Float()
+    month5 = fields.Float()
